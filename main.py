bba6f9f0b34ccfb4cc02d7c0178ecb795f952346
Stephen Jayakar <aj12ay@gmail.com>
2024-06-03 10:18:01 -0700

    added timestamp parsing

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class Message:
         self.timestamp = timestamp
 
     def __repr__(self):
-        return f'{self.sender}: {self.message}'
+        return f'{self.timestamp} {self.sender}: {self.message}'
 
 
 def get_senders() -> {}:
@@ -19,6 +19,21 @@ def get_senders() -> {}:
         alias, name = sender_line.split(',')
         senders[alias] = name
     return senders
+
+
+timestamp_regex = r'\w+ \d+, \d+  \d+:\d\d:\d\d (AM|PM)'
+
+
+def try_parse_timestamp(line) -> datetime:
+    match = re.match(timestamp_regex, line)
+    if match:
+        ts = match.group(0)
+        # TODO: no timezone, keeping it in PST
+        dt = datetime.strptime(ts, '%b %d, %Y  %I:%M:%S %p')
+        return dt
+    else:
+        return None
+
 
 def parse_txt(all_messages: str) -> list:
     # TODO: kind of clowny
@@ -33,7 +48,9 @@ def parse_txt(all_messages: str) -> list:
     sender = None
     message = None
     for line in all_messages.split('\n'):
-        
+        dt = try_parse_timestamp(line)
+        if dt:
+            messages.append(Message("Unknown", "Something", dt))
     return messages
 
 
