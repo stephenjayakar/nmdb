f5f6dd0158d55df18fea21912fae24189c445d97
Stephen Jayakar <aj12ay@gmail.com>
2024-06-08 15:05:25 -0700

    removed old search

--- frontend/src/convex/messages.ts
+++ frontend/src/convex/messages.ts
@@ -1,14 +1,6 @@
 import { query } from "./_generated/server";
 import { v } from "convex/values";
 
-export const search = query({
-  args: { searchTerm: v.string() },
-  handler: async (ctx, args) => {
-    const messages = await ctx.db.query("messages").collect();
-    return messages.filter((m) => m.message.includes(args.searchTerm));
-  },
-});
-
 export const fasterSearch = query({
   args: { searchTerm: v.string() },
   handler: async (ctx, args) => {
